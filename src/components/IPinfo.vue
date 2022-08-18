8d8f6502a387101c7146ac3574168f449ebecd22
shiekiragu12 <74773271+shiekiragu12@users.noreply.github.com>
2022-08-18 13:47:43 +0300

    Add files via upload

---
+++ src/components/IPinfo.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+</script>
+<template>
+<div class="absolute -bottom-64 lg: -bottom-20 flex flex-col lg:flex-row bg-white gap-y-4gap-x-12 px-8 pt-8 pb-12 rounded-md">
+<!--  Flex Item #1-->
+  <div class="flex flex-col">
+    <h3 class="text-xs mb-2 uppercase">IP Address</h3>
+  <span class="text-2xl">8.8.8.8</span>
+  </div>
+  <!--  Flex Item #2-->
+  <div class="flex flex-col">
+    <h3 class="text-xs mb-2 uppercase">LOcation</h3>
+  <span class="text-2xl">Michiang</span>
+  </div>
+  <!--  Flex Item #3-->
+  <div class="flex flex-col">
+    <h3 class="text-xs mb-2 uppercase">Timezone</h3>
+  <span class="text-2xl">UTC -07.00</span>
+  </div>
+  <!--  Flex Item #4-->
+  <div class="flex flex-col">
+    <h3 class="text-xs mb-2 uppercase">Isp</h3>
+  <span class="text-2xl">gOOGLE8</span>
+  </div>
+</div>
+</template>
+<style scoped>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex flex-col h-screen max-h-screen">
+<!--  Search/ Results-->
+    <div class="flex justify-center relative bg-hero-pattern bg-cover px-4 pt-8 pb-32">
+<!--    Search INput-->
+      <div class="w-full max-w-screen-sm">
+        <h1 class="text-white text-center text-3xl pb-4">
+          IP ADdress Tracker</h1>
+        <div class="flex">
+          <input class="flex-1 py-3 px-2 rounded-tl-md rounded-bl-md focus:outline-none" type="text"
+          placeholder="Search for any Ip address or leave empty to get your ip info">
+          <i class="cursor-pointer bg-black text-white px-4 rounded-tr-md rounded-br-md flex items-center bi bi-chevron-right"></i>
+        </div>
+      </div>
+      <!--    IP Info-->
+      <IPinfo/>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import IPinfo from "@/components/IPinfo";
+export default {
+  name: 'HomeView',
+  components: {
+    IPinfo
+  }
+}
+</script>
